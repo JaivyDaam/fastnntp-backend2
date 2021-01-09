37fcfba7c1f0dad173eaa1db2eadcf5329b386f2
Simon Schmidt <simon.schmidt@htp-tel.de>
2021-01-09 22:47:43 +0100

    Initial import

--- remote/mntp/buffer.go
+++ remote/mntp/buffer.go
@@ -1,5 +1,4 @@
-MIT License
-
+/*
 Copyright (c) 2021 Simon Schmidt
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -19,3 +18,42 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
+
+
+package mntpc
+
+
+import (
+	"io"
+	"sync"
+	"bytes"
+	"github.com/byte-mug/fastnntp"
+)
+
+
+type membuffer struct {
+	bytes.Buffer
+	writeToObject func(w *fastnntp.DotWriter)
+}
+
+var pool_membuffer = sync.Pool{ New: func() interface{} {
+	return new(membuffer)
+} }
+
+func getMembuf() *membuffer {
+	m := pool_membuffer.Get().(*membuffer)
+	if m.writeToObject==nil { m.writeToObject = m.writeTo }
+	return m
+}
+
+func (m *membuffer) release() {
+	m.Reset()
+	pool_membuffer.Put(m)
+}
+func (m *membuffer) writeTo(w *fastnntp.DotWriter) {
+	io.Copy(w,m)
+	m.release()
+}
+
+
--- remote/nntpclient/sync.go
+++ remote/nntpclient/sync.go
@@ -1,5 +1,4 @@
-MIT License
-
+/*
 Copyright (c) 2021 Simon Schmidt
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -19,3 +18,53 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
+
+
+package nntpclient
+
+import (
+	"sync"
+	"net/textproto"
+)
+
+
+type lPipeline struct {
+	textproto.Pipeline
+	rw sync.RWMutex
+}
+
+func (p *lPipeline) exclusive() func() {
+	p.rw.Lock()
+	return p.rw.Unlock
+}
+
+type lRequest struct {
+	*lPipeline
+	id uint
+	state uint
+}
+func (p *lPipeline) ackquire() (*lRequest) {
+	p.rw.RLock()
+	id := p.Next()
+	return &lRequest{p,id,0}
+}
+func (r *lRequest) update(state uint) {
+	if state>3 { state = 3 }
+	for r.state<state {
+		r.state++
+		switch r.state {
+		case 1: r.StartRequest(r.id)
+		case 2:
+			r.EndRequest(r.id)
+			r.StartResponse(r.id)
+		case 3:
+			r.EndResponse(r.id)
+			r.rw.RUnlock()
+		}
+	}
+}
+func (r *lRequest) req() { r.update(1) }
+func (r *lRequest) resp() { r.update(2) }
+func (r *lRequest) release() { r.update(3) }
+
